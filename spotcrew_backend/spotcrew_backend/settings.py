16e439ad3b9f8238226fdc935a3bc4da886971b1
dominik_krystkowiak <dominik.krystkowiak@nordcloud.com>
2019-04-07 15:03:34 +0200

    Fixed static settings

--- spotcrew_backend/spotcrew_backend/settings.py
+++ spotcrew_backend/spotcrew_backend/settings.py
@@ -112,6 +112,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
--- spotcrew_backend/spotcrew_backend/urls.py
+++ spotcrew_backend/spotcrew_backend/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    url(r'^docs/', include_docs_urls(title='Spotcrew API')),
+    url(r'^api/docs/', include_docs_urls(title='Spotcrew API')),
     url(r'^', include('events.urls')),
     url(r'^', include('tokens.jwt.urls')),
     url(r'^', include('tokens.users.urls')),
