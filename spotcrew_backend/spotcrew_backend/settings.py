0a1c4de2a89facb6ec1c21a812aa0e1e94afb60f
dominik_krystkowiak <dominik.krystkowiak@nordcloud.com>
2019-04-07 15:07:52 +0200

    Added static files directory

--- spotcrew_backend/spotcrew_backend/settings.py
+++ spotcrew_backend/spotcrew_backend/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
