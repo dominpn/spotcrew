40f86957123f6893587fa5d4786922ce747aae23
dominpn <dominik.krystkowiak@gmail.com>
2019-05-06 22:26:04 +0200

    Added settings

--- spotcrew_backend/spotcrew_backend/settings.py
+++ spotcrew_backend/spotcrew_backend/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'corsheaders',
     'rest_framework',
     'django_rest_passwordreset',
 
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
