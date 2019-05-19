3a632eadded4e243928a865e4d5ece25b6eeb03f
dominik_krystkowiak <dominik.krystkowiak@nordcloud.com>
2019-05-19 13:53:37 +0200

    Added images links to venues

--- spotcrew_backend/venues/api/serializers.py
+++ spotcrew_backend/venues/api/serializers.py
@@ -1,10 +1,42 @@
 from rest_framework_gis.serializers import GeoModelSerializer
 
+from images.api.serializers import ImageSerializer
+from images.models import Image
 from venues.models import Venue
 
 
 class VenueSerializer(GeoModelSerializer):
+    images = ImageSerializer(many=True, required=False)
+
     class Meta:
         model = Venue
         geo_field = 'location'
         fields = '__all__'
+
+    def create(self, validated_data):
+        images_links = validated_data.pop('images')
+        venue = Venue.objects.create(**validated_data)
+
+        for image_link in images_links:
+            image, created = Image.objects.get_or_create(link=image_link['link'])
+            venue.images.add(image)
+        return venue
+
+    def update(self, instance, validated_data):
+        images_links = validated_data.pop('images')
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.location = validated_data.get('location', instance.location)
+        instance.city = validated_data.get('city', instance.city)
+        instance.address = validated_data.get('address', instance.address)
+
+        images_list = []
+
+        for image in images_links:
+            link, created = Image.objects.get_or_create(link=image['link'])
+            images_list.append(link)
+
+        instance.images.set(images_list)
+        instance.save()
+        return instance
--- spotcrew_backend/venues/models.py
+++ spotcrew_backend/venues/models.py
@@ -1,6 +1,8 @@
 from django.contrib.gis.db import models as gis_models
 from django.db import models
 
+from images.models import Image
+
 
 class Venue(models.Model):
     venue_id = models.AutoField(primary_key=True)
@@ -9,6 +11,7 @@ class Venue(models.Model):
     location = gis_models.PointField()
     city = models.TextField()
     address = models.TextField()
+    images = models.ManyToManyField(Image)
     objects = gis_models.Manager()
 
     class Meta:
