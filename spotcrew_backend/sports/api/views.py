09497ffbba44f4df8b98c4a1a965e67381d3c611
Dominik Krystkowiak <dominik.krystkowiak@gmail.com>
2019-05-29 12:03:04 +0200

    Fixed deletion sports (#8)

--- spotcrew_backend/sports/api/views.py
+++ spotcrew_backend/sports/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, mixins
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
 
+from events.models import Event
 from sports.models import Sport
 from sports.api.serializers import SportSerializer
 
@@ -30,6 +32,12 @@ class SportDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Sport.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        Event.objects.filter(sport_id__sport_id=kwargs.get(self.lookup_field)).delete()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_serializer_context(self, *args, **kwargs):
         return {"request": self.request}
