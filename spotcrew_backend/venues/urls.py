f0609a0a4d1f0f98de201ef197faf1cfb77642d3
Dominik Krystkowiak <dominik.krystkowiak@gmail.com>
2019-05-29 12:37:00 +0200

    List events associated to venue (#10)
    * Added filter to events
    
    * Added new endpoint /api/venues/{pk}/events/

--- spotcrew_backend/venues/urls.py
+++ spotcrew_backend/venues/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from venues.api.views import VenueDetailView, VenueListView
+from venues.api.views import VenueDetailView, VenueListView, VenueEventsListView
 
 urlpatterns = [
     url(r'^api/venues/$', VenueListView.as_view(), name='list_venues'),
     url(r'^api/venues/(?P<pk>[0-9]+)$', VenueDetailView.as_view(), name='detail_venue'),
+    url(r'^api/venues/(?P<pk>[0-9]+)/events/$', VenueEventsListView.as_view(), name='list_venue_events')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
